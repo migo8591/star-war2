9135fb8bb650fce69b400e17db2c7faca128ca26
migo8591 <mcatedral24@hotmail.com>
2021-05-07 17:01:35 +0000

    avance2

--- src/js/views/detallesPersonas.js
+++ src/js/views/detallesPersonas.js
@@ -1,82 +1,105 @@
 import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
-import { Row, Col, Table, Media, Container, Button, Card } from "react-bootstrap";
 import { Context } from "../store/appContext";
-import personajes from "../../img/personajes.jpg";
-import "../../styles/detallesPersonas.scss";
 
-export const DetallesPersonas = props => {
-	const { id } = useParams();
-	const intId = parseInt(Id);
-	const history = useHistory;
+export const detallesPersonas = props => {
+	const { store, actions } = useContext(Context);
+	const params = useParams();
 
-	const goBack = () => {
-		history.goBack();
-	};
+	let nameStyle = { fontSize: "65px", marginTop: "100px", color: "#f6b26b" };
+
+	let columnStyle = { color: "#005073" };
+
+	let textStyle = { fontSize: "20px", marginTop: "35px" };
+
+	let rowStyle = { marginTop: "20px" };
+
+	let buttonStyle = { marginBottom: "20px", marginTop: "20px" };
 	return (
-		<Container>
-			<Media className="mt-5">
-				{store.planetsList.map((item, index) => {
-					if (index == intId) {
-						return (
-							<div key={index}>
-								<Row>
-									<Col>
-										<Card.Img variant="top" src={personajes} />
-									</Col>
-									<Col>
-										<Media.Body className="text-center">
-											<h5>{each.name}</h5>
-											<p>
-												During the Clone Wars, a coup orchestrated by Death Watch topples the
-												pacifist regime of the New Mandalorians and returns the armored warrior
-												culture to a position of power over the isolated world. What the average
-												Mandalorian citizen does not realize, however, is that the takeover is
-												in truth masterminded by a secret syndicate of criminals the Shadow
-												Collective led by Maul. Mauls loyal Mandalorians modify their armor to
-												reflect allegiance to the Dark Lord. These super commandos wear armor of
-												red and black, and some even fashion horns atop their helmets, to better
-												resemble their Nightbrother leader. During the Siege of Mandalore, Gar
-												Saxon leads the elite fighting force.
-											</p>
-										</Media.Body>
-									</Col>
-								</Row>
-								<Table responsive="sm">
-									<thead>
-										<tr>
-											<th>Name</th>
-											<th>Birth Year</th>
-											<th>Gender</th>
-											<th>Heigth</th>
-											<th>Skin Color</th>
-											<th>Eye Color</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>{each.name}</td>
-											<td>{each.birth_year}</td>
-											<td>{each.gender}</td>
-											<td>{each.height}</td>
-											<td>{each.skin_color}</td>
-											<td>{each.eye_color}</td>
-										</tr>
-									</tbody>
-								</Table>
-								<Button variant="dark" onClick={() => goBack()}>
-									Go Back
-								</Button>
-							</div>
-						);
-					}
-				})}
-			</Media>
-		</Container>
+		<div className="container mt-5">
+			<div className="row">
+				<div className="col-sm">
+					<img
+						width={500}
+						height={500}
+						className="align-self-center mr-3"
+						src={personajes}
+						alt="Generic placeholder"
+					/>
+				</div>
+				<div className="col-sm text-center">
+					<h5 style={nameStyle}> {store.perros[params.theid].name}</h5>
+					<p style={textStyle}>{store.perros[params.theid].history}</p>
+					<p style={textStyle}>{store.perros[params.theid].other}</p>
+				</div>
+				<div className="container mt-5">
+					<div className="row d-flex align-items-center" style={columnStyle}>
+						<div className="col-2 text-center">
+							<h4>Nombre</h4>
+						</div>
+						<div className="col-2 text-center">
+							<h4>Sexo</h4>
+						</div>
+						<div className="col-2 text-center">
+							<h4>Edad</h4>
+						</div>
+						<div className="col-2 text-center">
+							<h4>Comportamiento</h4>
+						</div>
+						<div className="col-2 text-center">
+							<h4>Raza</h4>
+						</div>
+						<div className="col-2 text-center">
+							<h4>Tamaño</h4>
+						</div>
+					</div>
+					<hr className="my-4 bg-warning" />
+					<div className="row d-flex align-items-center" style={rowStyle}>
+						<div className="col-2 text-center">
+							<h5>{store.perros[params.theid].name}</h5>
+						</div>
+						<div className="col-2 text-center">
+							<h5>{store.perros[params.theid].sex}</h5>
+						</div>
+						<div className="col-2 text-center">
+							<h5>{store.perros[params.theid].age}</h5>
+						</div>
+						<div className="col-2 text-center">
+							<h5>{store.perros[params.theid].behaviour}</h5>
+						</div>
+						<div className="col-2 text-center">
+							<h5>{store.perros[params.theid].breed}</h5>
+						</div>
+						<div className="col-2 text-center">
+							<h5>{store.perros[params.theid].size}</h5>
+						</div>
+					</div>
+				</div>
+
+				<div className="container">
+					<div className="row">
+						<div className="col-9">
+							<Link to="/">
+								<button type="button" className="btn btn-primary mr-2" style={buttonStyle}>
+									Regresar
+								</button>
+							</Link>
+						</div>
+						<div className="col-3 d-flex justify-content-end">
+							<Link to="/adoptform">
+								<button type="button" className="btn btn-primary" style={buttonStyle}>
+									Formulario de adopción
+								</button>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
-// Petdetail.propTypes = {
-// 	match: PropTypes.object
-// };
+Dogdetail.propTypes = {
+	match: PropTypes.object
+};
