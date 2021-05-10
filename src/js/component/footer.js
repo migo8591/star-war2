1b088a813bbbbd1cb5faad52bcfbbe9d813cadc0
migo8591 <mcatedral24@hotmail.com>
2021-05-10 00:11:41 +0000

    terminado con footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -5,8 +5,8 @@ import starlogo from "../../img/starlogo.png";
 
 export const Footer = () => {
 	return (
-		<div className="text-center mt-5 d-flex flex-column container-flux bg-warning">
-			<h1 />
+		<div className="text-center mt-5 d-flex flex-column  bg-warning">
+			<h4>Muchas gracias teachers!</h4>
 		</div>
 	);
 };
